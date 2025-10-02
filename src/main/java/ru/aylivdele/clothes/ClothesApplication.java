b2281327bd37c3a99ecced702be26ae52b2f14a4
aylivdele <irassomahin@gmail.com>
2025-10-02 13:37:18 +0500

    backend init

---
+++ src/main/java/ru/aylivdele/clothes/ClothesApplication.java
@@ -0,0 +1,13 @@
+package ru.aylivdele.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/aylivdele/clothes/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package ru.aylivdele.clothes.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilter(
+            HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.HEAD).permitAll()
+                        .requestMatchers("/api/**").hasRole("ADMIN"))
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
